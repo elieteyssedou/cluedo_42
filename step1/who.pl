7e382cf3d097939a5c61da68c70ae98e6a42796f
Elie <elieteyssedou@gmail.com>
2015-03-28 17:42:49 +0100

    aaa

--- step1/who.pl
+++ step1/who.pl
@@ -38,3 +38,9 @@ enfant_de(valerie, loic).
 
 enfant_de(X, Z) :- mari_de(Y, Z), enfant_de(X, Y).
 femme_de(Femme, Mari) :- mari_de(Mari, Femme).
+beaupere_de(B, X):- homme(B), mari_de(X, F), enfant_de(F, B).
+beaupere_de(B, X):- homme(B), femme_de(X, M), enfant_de(M, B).
+bellemere_de(B, X):- femme(B), mari_de(M, X), enfant_de(M, B).
+bellemere_de(B, X):- femme(B), femme_de(F, X), enfant_de(F, B).
+ancetre_de(A, B) :- enfant_de(B, A).
+ancetre_de(A, C) :- enfant_de(B, A), ancetre_de(B, C).
